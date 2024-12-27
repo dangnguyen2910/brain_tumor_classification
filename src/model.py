6245429c78b2c36a0a5f4940d1224f3360d0049f
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2024-12-27 10:03:33 +0700

    Fix forward function in model class

--- src/model.py
+++ src/model.py
@@ -1,12 +1,18 @@
 import torch 
 from torchvision.models import resnet18
 
-class Model: 
+class Model(torch.nn.Module):
     def __init__(self, n_class): 
+        torch.nn.Module.__init__(self)
         self.model = resnet18(weights=None)
         self.model.fc = torch.nn.Linear(in_features=512, out_features=n_class)
+
 
     def forward(self, x): 
         return self.model(x)
 
 
+# if __name__ == "__main__":
+#     model = Model(n_class = 2)
+#     input = torch.rand(1,3,512,512)
+#     print(model(input))
